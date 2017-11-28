ed5f60dc51224d1bf7297bde281916293241b8a3
Peter Goshulak <pgoshulak@gmail.com>
2017-11-27 19:52:22 -0500

    Add sites; add section for non-working sites

--- content.js
+++ content.js
@@ -21,47 +21,66 @@ const selectorPairs = [
     // https://www.foodiecrush.com/spiralized-butternut-squash-and-apple-salad-with-turkey/
     show: '.easyrecipe',
     hide: '.entry_content'
-  }/* ,
+  },
   {
-    // gimmesomeoven.com
-    show: '.recipe',
-    hide: '.post'
-  } ,
+    // http://www.abbeyskitchen.com/recipe-gluten-dairy-free-sticky-figgy-gingerbread-loaf/
+    show: '.wprm-recipe-container'
+  },
   {
-    // smittenkitchen.com
-    show: '.tasty-recipes, .tasty-recipes-image-shim',
-    hide: '.entry-content'
-  } */
+    // https://www.vibrantplate.com/crispy-berries-plum-crumble-vegan/
+    show: '.zlrecipe-container-border'
+  },
+  {
+    // https://www.delishknowledge.com/the-best-vegan-stuffing/
+    show: '.cookbook-recipe'
+  },
+  {
+    // https://naturallyella.com/pumpkin-ricotta-crostini/
+    show: '.simple-recipe-pro, .recipe-container'
+  },
+  {
+    // http://www.foodheavenmadeeasy.com/vegetable-collard-wraps-with-peanut-sauce/
+    show: '.blog-yumprint-recipe'
+  },
+  {
+    // https://www.101cookbooks.com/archives/a-simple-carrot-soup-recipe.html
+    show: '#recipe'
+  },
+  {
+    // https://www.mynewroots.org/site/2017/10/celebrating-10-years-carrot-cake/
+    show: '.print-this-content'
+  },
+  {
+    // http://ohsheglows.com/2013/10/21/smoky-butternut-squash-sauce-with-pasta-and-greens/
+    show: '.recipe-content'
+  },
+  {
+    // http://kblog.lunchboxbunch.com/2017/10/chocolate-chip-pumpkin-loaf-vegan.html TODO: see 'non-working' below
+    show: '.hrecipe'
+  }
 ]
 
-addClass = (selector, classes) => {
-  // document.querySelector(selector).classList.add(...classes.split(' '));
-  let elems = document.querySelectorAll(selector),
-    len = elems !== null ? elems.length : 0,
-    i = 0;
-  
-  for (i; i < len; i++) {
-    elems[i].className += " " + classes
-  }
-  console.log(`--- Found ${len} instances of ${selector} --> added ${classes}`)
-  return len
-}
+/* Non-working sites (links are sample posts)
+--- Single elem to hide ---
+https://smittenkitchen.com/2008/03/vegetarian-cassoulet/ --> single div.print-hide, no div.recipe but many p's
+https://deliciouslyella.com/2016/01/23/book-two-recipe-chickpea-quinoa-and-turmeric-curry/ --> .blog-post__recipe-description
+https://www.saveur.com/sri-lankan-fish-curry-recipe#page-2
+- div.recipe is one layer deep
 
-addClassToElement = (elem, className) => {
-  elem.className += " " + className
-}
+--- Multi elems to hide ---
+http://kblog.lunchboxbunch.com/2017/11/shredded-brussels-sprouts-salad-with.html
+- multiple of same div.hrecipe but one inside comments section (?!?!)
+- only 1st is visible, other's parent is 'display:none'
+
+--- Probably impossible ---
+http://www.greenkitchenstories.com/green-peanut-butter-sandwich-smoothie/ --> just a bunch of p's, no good way to discern
+http://thefirstmess.com/2012/02/19/almond-sweet-potato-biscuits-mushroom-gravy/ --> bunch of p's
+*/
 
 findElemsToShow = (selectorPair) => {
   let query = `${selectorPair.show}`
   // console.log(`Matching for ${selectorPair.show}...`)
   // console.log(document.querySelectorAll(query))
-  return document.querySelectorAll(query)
-}
-
-findElemsToHide = (selectorPair) => {
-  let query = `${selectorPair.hide}:not(${CSS_HIDE_CLASS})`
-  console.log(`Matching for ${selectorPair.hide}...`)
-  console.log(document.querySelectorAll(query))
   return document.querySelectorAll(query)
 }
 
@@ -82,6 +101,8 @@ findElemSiblings = (elems) => {
     })
 
     if (elemsAreSiblings) {
+      console.log('=== parentTest.Children ===')
+      console.log(parentTest.children)
       // Proceed similarly to case where only 1 elem (above), but matching all recipe elems
       return Array.prototype.filter.call(parentTest.children, (child) => {
         return !elems.includes(child)
